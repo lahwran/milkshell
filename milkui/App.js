5672596bc54adcbadf033c4030030bfa883195bb
Lauren Chris Horne <lahwran0@gmail.com>
2019-08-05 16:28:57 -0700

    initial remote json

--- milkui/App.js
+++ milkui/App.js
@@ -13,6 +13,7 @@ import {
   ScrollView,
   View,
   Text,
+  TextInput,
   StatusBar,
 } from 'react-native';
 
@@ -24,8 +25,89 @@ import {
   ReloadInstructions,
 } from './NewAppScreen/index';
 
-const App = () => {
-  return (
+// TODO: POTENTIAL SECURITY VULNERABILITY
+// remote code can invoke any attribute of funcs :s
+// how 2 hashmap
+// TODO: use more efficient protocol
+// TODO: check missing/invalid fields
+// TODO: print names of fields in errors
+// TODO: merge typehandlers and unpackers? do we want to? typehandlers are more checked
+function handler(instantiate, attrs) {
+  return {
+    isHandler: true, // TODO: to ensure we don't accidentally read from __proto__, can we do better?
+    instantiate, // func to convert
+    unpacker: (params, sd) => { // todo: make this into a more general wrapper?
+      console.log("handler unpacker", params, sd)
+      return attrs.reduce(
+        (accum, [name, u]) => ({...accum, [name]: u(params[name], sd)}),
+        {}
+      )
+    }
+  }
+}
+
+class UnpackException extends Error {}
+
+// type specifications
+const unpack = {
+  integer: (val) => parseInt(val),
+  string: (val) => val, // TODO: check type
+  array: (val, sd) => {
+    console.log(`unpack array`, val, sd);
+    return val.map((item, index) => unpack.tagged(item, sd, {index}))
+  },
+  tagged: ([type, val], sd) => {
+    const handler = typehandlers[`${type}__type`]
+    if (!handler || !handler.isHandler) { throw new UnpackException(`nonexistant type: '${type}'`) }
+    const unpacked_val = handler.unpacker(val, sd);
+    console.log(`unpack "${type}"`, val, sd, typehandlers, "unpacked val:", unpacked_val)
+    return handler.instantiate(unpacked_val);
+  }
+}
+
+// type specification annotations
+const wrappers = {
+  required: (unpacker, allowNull=false) => (val, sd) => {
+    if (val === null && !allowNull) {
+      throw new UnpackException(`null not allowed here`)
+    } else if (val === undefined) {
+      throw new UnpackException(`field is required`)
+    }
+    return unpacker(val, sd);
+  },
+  optional: (unpacker, def=undefined, allowNull=false) => (val, sd) => {
+    if (val === null && !allowNull) {
+      throw new UnpackException(`null not allowed here`)
+    }
+    if (val === undefined || val === null) {
+      return def
+    }
+    return unpacker(val, sd);
+  }
+}
+// public obj type handlers
+const typehandlers = {
+  // postfixed with type__ to prevent possible security issue. I hate javascript, I wish I understood guarantees about object attrs better
+  stream__type: handler((params, streamdispatch) => (message) => streamdispatch(params, message), [
+    ["sid", wrappers.required(unpack.integer)]
+  ]),
+  view__type: handler(({children, ...p}) => <View {...p}>{children}</View>, [
+    ["children", wrappers.optional(unpack.array)]
+  ]),
+  plaintext__type: handler(({value}) => <Text style={styles.sectionDescription}>{value}</Text>, [
+    ["value", wrappers.required(unpack.string)]
+  ]),
+  //string__type: handler(({value}) => <Text>{value}</Text>, [
+  //  ["value", wrappers.required(unpack.string)]
+  //])
+  //// seems a little dumb to duplicate this
+  //integer__type: handler(({value}) => {value}, [
+  //  ["value", wrappers.required(unpack.integer)]
+  //])
+};
+
+
+/* original code: (
     <Fragment>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
@@ -69,7 +151,21 @@ const App = () => {
         </ScrollView>
       </SafeAreaView>
     </Fragment>
-  );
+    )*/
+
+const streamdispatch = (arg1, arg2) => console.log(arg1, arg2)
+const App = () => {
+  const tree = ["view", {
+    "children": [
+      ["plaintext", {value: "hello world"}]
+    ]
+  }];
+  return <Fragment>
+    <StatusBar barStyle="dark-content" />
+    <SafeAreaView>
+      {unpack.tagged(tree, streamdispatch)}
+    </SafeAreaView>
+  </Fragment>
 };
 
 const styles = StyleSheet.create({
