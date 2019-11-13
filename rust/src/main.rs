1c1b88d0741f3f77271fa96795af66050b57d836
Lauren Chris Horne <lahwran0@gmail.com>
2019-11-13 10:34:20 -0800

    Rust parser wip

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,29 @@
+
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+use pest::iterators::Pair;
+
+#[derive(Parser)]
+#[grammar = "milkshell.pest"]
+pub struct MilkshellParser;
+
+fn walk(pair: Pair<Rule>, depth: usize) {
+    println!("{}{:?}: {}", " ".repeat(depth*2), pair.as_rule(), pair.as_str());
+    for _x in pair.into_inner().map(|x| { walk(x, depth+1) } ) {}
+}
+
+fn parse_milkshell(file: &str) {
+    println!("start");
+    let ms = MilkshellParser::parse(Rule::milkshell, file).expect("Parsing Failed").next().unwrap();
+    println!("done parsing");
+    walk(ms, 0);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s = "derp herp     herk 'derp derp' herp | 'derp derp derp' \n | derk derk derk | derp | derpderp|derp| derp  derp| derp derp\n\n";
+    parse_milkshell(s);
+
 }
